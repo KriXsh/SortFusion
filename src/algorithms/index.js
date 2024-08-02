20cea8720e51004c4a08e75a7580cc93b573b03f
krish <krishnendughosal999@gmail.com>
2024-08-02 13:35:37 +0530

    selection sort added/ optimzed code

---
+++ src/algorithms/index.js
@@ -0,0 +1,12 @@
+// src/algorithms/index.js
+const { mergeSort } = require('./mergeSort');
+const { quickSort } = require('./quickSort');
+const { bubbleSort } = require('./bubbleSort');
+const { selectionSort } = require('./selectionSort');
+
+module.exports = {
+    mergeSort,
+    quickSort,
+    bubbleSort,
+    selectionSort
+};
---
+++ src/algorithms/selectionSort.js
@@ -0,0 +1,20 @@
+// src/algorithms/selectionSort.js
+const selectionSort = (array) => {
+    const n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [array[i], array[minIndex]] = [array[minIndex], array[i]];
+        }
+    }
+    return array;
+};
+
+module.exports = {
+    selectionSort
+};
